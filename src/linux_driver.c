28d872468101983e62ae845fe07d4144b2ac7b53
alkuzin <alkuzindev@gmail.com>
2024-06-01 16:41:11 +0300

    feat: added prototypes for character device file operations

--- src/linux_driver.c
+++ src/linux_driver.c
@@ -18,16 +18,20 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/init.h>
+#include <linux/fs.h>
 
-/* define module ring buffer size param */
-static s32 buffer_size = 1024;
-/* S_IRUGO - read permissions for the owner, group, and others */
-module_param(buffer_size, int, S_IRUGO);
+
+#define DEVICE_NAME "test_task_dev"
 
 /* module info */
 MODULE_LICENSE("GPL v3");
 MODULE_AUTHOR("Alexander (@alkuzin)");
 MODULE_DESCRIPTION("Character device driver for inter-process communication");
+
+/* define module ring buffer size param */
+static s32 buffer_size = 1024;
+/* S_IRUGO - read permissions for the owner, group, and others */
+module_param(buffer_size, int, S_IRUGO);
 
 /**
  * @brief Driver entry point. 
@@ -35,22 +39,87 @@ MODULE_DESCRIPTION("Character device driver for inter-process communication");
  * @return 0 - in case of success. 
  * @return -1 - in case of error. 
  */
-static s32 driver_init(void);
+static s32 __init driver_init(void);
 
 /** @brief Driver exit handler. */
-static void driver_exit(void);
+static void __exit driver_exit(void);
+
+/**
+ * @brief Open function for character device.
+ * 
+ * @param [in] inode - given inode structure.
+ * @param [in] file - given file structure.
+ * @return 0 - in case of success. 
+ * @return -1 - in case of error. 
+ */
+static s32 dev_open(struct inode *inode, struct file *file);
+
+/**
+ * @brief Release function for character device.
+ * 
+ * @param [in] inode - given inode structure.
+ * @param [in] file - given file structure.
+ * @return 0 - in case of success. 
+ * @return -1 - in case of error. 
+ */
+static s32 dev_release(struct inode *inode, struct file *file);
+
+/**
+ * @brief Read data from the character device.
+ *
+ * @param [in] file - given file structure.
+ * @param [out] buffer - given buffer to store the read data.
+ * @param [in] length - given number of bytes to read.
+ * @param [in] offset - given file offset.
+ * @return Number of bytes read on success, or an error code on failure.
+ */
+static ssize_t dev_read(struct file *file, char *buffer, size_t length, loff_t *offset);
+
+/**
+ * @brief Write data to the character device.
+ *
+ * @param [in] file - given file structure.
+ * @param [in] buffer - given buffer of data to be written.
+ * @param [in] length - given number of bytes to write.
+ * @param [in] offset - given file offset.
+ * @return Number of bytes written on success, or an error code on failure.
+ */
+static ssize_t dev_write(struct file *file, const char *buffer, size_t length, loff_t *offset);
+
+/** @brief Set of operations that can be performed on a character device in the kernel. */
+static struct file_operations fops = {
+    .open    = dev_open,
+    .release = dev_release,
+    .read    = dev_read,
+    .write   = dev_write
+};
+
+static char buffer[buffer_size];
+static s32  major_number;
 
 
-static s32 driver_init(void)
+static s32 __init driver_init(void)
 {
     printk(KERN_INFO "%s\n", "linux_driver: initialization");
+
+    major_number = register_chrdev(0, DEVICE_NAME, &fops);
+
+    if (major_number < 0) {
+        printk(KERN_ALERT "Failed to register a major number. Status code: %d\n", major_number);
+        return major_number;
+    }
+
+    printk(KERN_INFO "linux_driver: registered correctly with major number: %d \n", major_number);
     printk(KERN_INFO "linux_driver: set the ring buffer size to %d bytes\n", buffer_size);
+
     // TODO: implement driver
     return 0;
 }
 
-static void driver_exit(void)
+static void __exit driver_exit(void)
 {
+    unregister_chrdev(major_number, DEVICE_NAME);
+    printk(KERN_INFO "%s\n", "linux_driver: unregister character device");
     printk(KERN_INFO "%s\n", "linux_driver: exit");
 }
 
